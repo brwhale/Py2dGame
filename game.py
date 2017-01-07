d536903d631e1d2f38bec2128ba607df9609e779
Garrett Skelton <garrett.skelton@guidantfinancial.com>
2017-01-06 16:37:00 -0800

    first commit

---
+++ game.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame.locals import *
+import events
+
+class App(events.CEvent):
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 1280, 720
+ 
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+        self._image_surf = pygame.image.load("tex/glider.jpg").convert()
+    def on_loop(self):
+        pass
+    def on_render(self):
+        self._display_surf.blit(self._image_surf,(0,0))
+        pygame.display.flip()
+    def on_cleanup(self):
+        pygame.quit()
+    def on_exit(self):
+        self._running = False
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+ 
+if __name__ == "__main__" :
+    theApp = App()
+    theApp.on_execute()
